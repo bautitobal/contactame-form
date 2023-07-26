969eb34c83033b6a3af832ed0e57e23ea5d554b1
Bauti <bautitobal11@gmail.com>
2023-07-25 23:13:54 -0300

    adding js code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-ECHO is on.
+// Obtener referencia al campo de entrada de teléfono
+const phoneInput = document.getElementById("phone");
+
+// Agregar un listener para el evento "input"
+phoneInput.addEventListener("input", function (event) {
+  // Obtener el valor ingresado por el usuario
+const inputValue = event.target.value;
+
+  // Expresión regular que permite solo dígitos numéricos
+const numericRegex = /^\d*$/;
+
+  // Validar si el valor ingresado no contiene solo dígitos numéricos
+if (!numericRegex.test(inputValue)) {
+    // Si contiene caracteres no numéricos, eliminarlos del valor
+    event.target.value = inputValue.replace(/\D/g, "");
+}
+});
